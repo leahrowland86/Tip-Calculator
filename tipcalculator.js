594de7387f17f4e4346c1119f0ea62f55b666514
leahrowland86 <50224313+leahrowland86@users.noreply.github.com>
2019-05-07 20:12:17 -0700

    Add files via upload

--- tipcalculator.js
+++ tipcalculator.js
@@ -1,40 +1,42 @@
 let billInput = document.querySelector("#billamount");
 let tipInput = document.querySelector("#tippercentage");
+let otherTipInput = document.querySelector("#othertipamount");
 // let button = document.querySelector("#calculate");
 
 function calculateTip() {
+  // debugger;
   var billAmount = Number(document.getElementById("billamount").value);
   var tipPercentage = Number(document.getElementById("tippercentage").value);
+
+      var otherTipAmount = Number(document.getElementById("othertipamount").value);
 
   var textBox = Number(document.getElementById("billamount").value);
 
   var total = (billAmount * tipPercentage);
 
+        var otherTotal = (otherTipAmount / 100) * billAmount;
+
   var totalBill = (total + billAmount);
 
-  document.getElementById("tip").innerHTML = "$" + total + " = ";
+        var otherTotalBill = (otherTotal + billAmount);
 
   document.getElementById("bill").innerText = "$" + billAmount  + " + ";
 
+  document.getElementById("tip").innerHTML = "$" + total + " = ";
+
+        document.getElementById("tipother").innerHTML = "$" + otherTotal + " = ";
+
   document.getElementById("totalDue").innerText = "$" + totalBill;
+
+        document.getElementById("otherTotalDue").innerText = "$" + otherTotalBill;
 };
-
-// function verifyTip() {
-//   if (tipPercentage == 0)
-//   {
-//     document.getElementById("otherTipAmount").innerHTML = "$" + totalOther + " = ";
-//   } else {
-//   document.getElementById("tip").innerHTML = "$" + total + " = ";
-// }
-// };
-
-// button.addEventListener('click', function() {
-//   calculateTip();
-// });
 
 billInput.addEventListener('input', function() {
   calculateTip();
 });
 tipInput.addEventListener('input', function() {
+  calculateTip();
+});
+otherTipInput.addEventListener('input', function() {
   calculateTip();
 });

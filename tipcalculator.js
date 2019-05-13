fcff6f4aeef1d3fd6cdbb0f2874cf4d92e16efb9
leahrowland86 <50224313+leahrowland86@users.noreply.github.com>
2019-05-12 17:51:52 -0700

    Add files via upload

--- tipcalculator.js
+++ tipcalculator.js
@@ -1,4 +1,4 @@
-function billAmount() {
+function billCalcAmount() {
 //associate the bill amount entered by the user with billAmount variable
   let billAmount = Number(document.getElementById("billamount").value);
 
@@ -6,9 +6,9 @@ function billAmount() {
   document.getElementById("bill").innerHTML = "$" + billAmount + " + ";
 
   return billAmount;
-};
+}
 
-function tipAmount() {
+function tipCalcAmount() {
   let billAmount = Number(document.getElementById("billamount").value);
   let tipPercent = Number(document.getElementById("tippercentage").value);
   let tipAmount = (billAmount * tipPercent);
@@ -16,32 +16,111 @@ function tipAmount() {
   document.getElementById("tip").innerHTML = "$" + tipAmount + " = ";
 
   return tipAmount;
-};
+}
 
-function totalAmount() {
+function totalCalcAmount() {
   let billAmount = Number(document.getElementById("billamount").value);
   let tipPercent = Number(document.getElementById("tippercentage").value);
   let tipAmount = (billAmount * tipPercent);
-  let totalCalcAmount = (billAmount + tipAmount);
+  let totalAmount = (billAmount + tipAmount);
 
-  document.getElementById("total").innerHTML = "$" + totalCalcAmount;
+  document.getElementById("total").innerHTML = "$" + totalAmount + "select";
 
-  return totalCalcAmount;
-};
+  return totalAmount;
+}
 
+function otherTipCalcPercentAmount() {
+  debugger;
+  let billAmount = Number(document.getElementById("billamount").value);
+  let tipTypePercent = document.getElementById("othertipamountpercent").checked;
+  let tipAmountPercent = Number(document.getElementById("tipamountpercent").value);
+  let tipPercent = ((tipAmountPercent / 100) * billAmount);
+  let totalAmount = (billAmount + tipPercent);
+
+  if (tipTypePercent.checked = true) {
+  document.getElementById("tipamountpercent").disabled = false;
+}
+
+  if (tipTypePercent.checked = true) {
+  document.getElementById("othertip1").innerHTML = "$" + tipPercent;
+  } ""
+
+  document.getElementById("totalpercent").innerHTML = "$" + totalAmount + "percent";
+
+  return totalAmount;
+}
+
+function otherTipCalcDollarAmount() {
+  let billAmount = Number(document.getElementById("billamount").value);
+  let tipTypeDollar = document.getElementById("othertipamountdollar").checked;
+  let tipAmountDollar = Number(document.getElementById("tipamountdollar").value);
+  let totalAmount = (tipAmountDollar + billAmount);
+
+  if (tipTypeDollar.checked = true) {
+  document.getElementById("othertip2").innerHTML = "$" + tipAmountDollar;
+  } ""
+
+  if(tipTypeDollar.checked = true) {
+    document.getElementById("tipamountdollar").disabled = false;
+  }
+
+  document.getElementById("totaldollar").innerHTML = "$" + totalAmount + "dollar";
+
+  return totalAmount;
+}
 
 //listen for the user's bill amount and output it automatically and CALLS the function
 let billInput = document.querySelector("#billamount");
 let totalInput = document.querySelector("#total");
 billInput.addEventListener('input', function() {
-  billAmount();
-  totalAmount();
-  tipAmount();
+  billCalcAmount();
+  totalCalcAmount();
+  tipCalcAmount();
+  otherTipCalcPercentAmount();
+  otherTipCalcDollarAmount();
 });
 
 let tipInput = document.querySelector("#tippercentage");
 tipInput.addEventListener('input', function() {
-  tipAmount();
-  billAmount();
-  totalAmount();
+  tipCalcAmount();
+  billCalcAmount();
+  totalCalcAmount();
+  otherTipCalcPercentAmount();
+  otherTipCalcDollarAmount();
+});
+
+let otherPercentTipInput = document.querySelector("#tipamountpercent");
+otherPercentTipInput.addEventListener('input', function() {
+  tipCalcAmount();
+  billCalcAmount();
+  totalCalcAmount();
+  otherTipCalcPercentAmount();
+  otherTipCalcDollarAmount();
+});
+
+let otherDollarTipInput = document.querySelector("#tipamountdollar");
+otherDollarTipInput.addEventListener('input', function() {
+  tipCalcAmount();
+  billCalcAmount();
+  totalCalcAmount();
+  otherTipCalcPercentAmount();
+  otherTipCalcDollarAmount();
+});
+
+let percentTipInput = document.querySelector("#othertipamountpercent");
+percentTipInput.addEventListener('input', function() {
+  tipCalcAmount();
+  billCalcAmount();
+  totalCalcAmount();
+  otherTipCalcPercentAmount();
+  otherTipCalcDollarAmount();
+});
+
+let dollarTipInput = document.querySelector("#othertipamountdollar");
+percentTipInput.addEventListener('input', function() {
+  tipCalcAmount();
+  billCalcAmount();
+  totalCalcAmount();
+  otherTipCalcPercentAmount();
+  otherTipCalcDollarAmount();
 });
